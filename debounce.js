2a43c91aa32410f7f2202fdf0d253a1c3a6f542d
mar <mar.nospam@anomy.net>
2017-09-13 10:44:21 +0000

    refactor: Recombine code-paths for `immediate` flag

--- debounce.js
+++ debounce.js
@@ -3,26 +3,16 @@
 // the returned function also has a nice .cancel() method.
 const debounce = (func, delay, immediate) => {
   let timeout;
-  const debouncedFn = !immediate ?
-      // simple delayed function
-      function (...args) {
-        const _this = this;
-        clearTimeout(timeout);
-        timeout = setTimeout(() => {
-          func.apply(_this, args);
-        }, delay);
-      }:
-      // more complex immediately called function
-      function (...args) {
-        const runNow = !timeout && immediate;
-        const _this = this;
-        clearTimeout(timeout);
-        timeout = setTimeout(() => {
-          !runNow  &&  func.apply(_this, args); // don't re-invoke `func` if runNow is true
-          timeout = 0;
-        }, delay);
-        runNow  &&  func.apply(_this, args);
-      };
+  const debouncedFn = function (...args) {
+    const runNow = immediate && !timeout;
+    const _this = this;
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      !runNow  &&  func.apply(_this, args); // don't re-invoke `func` if runNow is true
+      timeout = 0;
+    }, delay);
+    runNow  &&  func.apply(_this, args);
+  };
   debouncedFn.cancel = () => {
     clearTimeout(timeout);
     timeout = 0;
