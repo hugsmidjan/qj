9075a02d33f4a2cdfcc9fb2cf01b6076c68be836
mar <mar.nospam@anomy.net>
2017-10-06 11:17:14 +0000

    feat: make `onNext`/`onEvery` canceller more explicit.

--- onNext.js
+++ onNext.js
@@ -1,9 +1,8 @@
 /*
   Executes callback when the clock strikes the next whole `periodSizeMs`
-  Returns a canceller function - that allows for optional execution of the callback.
+  Returns an object with a `cancel` function - which optionally executes the callback.
 
   Usage:
-
       const MINUTE = 60*1000;
       const HOUR = 60*MINUTE;
 
@@ -14,10 +13,10 @@
       const at12_15 = onNext(12*HOUR, 15*MINUTE, () => console.log(Date()) );
 
       // Cancel the 12 o'clock callback:
-      at12();
+      at12.cancel();
 
-      // Cancel the 12:15 schedule and run the callback right now!
-      at12_15(true);
+      // Nevermind the 12:15 schedule. Run the callback right now!
+      at12_15.cancel(true);
 
 */
 export default function onNext(periodSizeMS, offsetMs, callback) {
@@ -26,15 +25,16 @@ export default function onNext(periodSizeMS, offsetMs, callback) {
     offsetMs = 0;
   }
   const msToNext = periodSizeMS - (Date.now() - offsetMs) % periodSizeMS;
-  // add a slight .5% - 1% fuzz to the timer to avoid
+  // OPINIONATED: Add a slight .5% - 1% fuzz to the timer to avoid
   // A) crazy spikes in server-load (in case of multiple clients)
   // B) accidental under-shoots caused by bad timer handling in the browser
   const fuzz = (1 + Math.random()) * Math.max(.01*periodSizeMS, 100);
   const timeout = setTimeout(callback, msToNext + fuzz);
 
-  const canceller = (execCallback) => {
-    clearTimeout(timeout);
-    execCallback && callback();
+  return {
+    cancel: (execCallback) => {
+      clearTimeout(timeout);
+      execCallback && callback();
+    },
   };
-  return canceller;
 }
