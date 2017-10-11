a534f9708b4b8ea09c20003fdead3929885e167c
mar <mar.nospam@anomy.net>
2017-10-11 16:00:57 +0000

    feat: Add `objectIsSame` and `objectReplace` methods

--- object.js
+++ object.js
@@ -3,6 +3,12 @@
 // Small, fast, stupid, practical, & care-free.
 // False-positives (like NaN and 0 v -0) considered acceptable.
 
+// INFO: Interesting 3rd party deep equals helpers.
+//  * https://github.com/ReactiveSets/toubkal/blob/master/lib/util/value_equals.js
+//    (https://github.com/ReactiveSets/toubkal/blob/1b73baf288385b34727ddf6d223f62c3bb2cb176/lib/util/value_equals.js)
+//  * https://github.com/lodash/lodash/blob/es/_baseIsEqual.js
+//   (https://github.com/lodash/lodash/blob/f71a7a04b51bd761683e4a774c5b1d38bdaa7b20/_baseIsEqual.js)
+
 // import './polyfills/Object.assign';
 
 // IE11 compatible no-polyfill object cloner
@@ -71,6 +77,22 @@ const objectIsEmpty = (object) => {
 };
 
 
+// Returns true if objects a and b contain 100% the same values.
+const objectIsSame = (a, b) => {
+  for (const key in b) {
+    if ( a[key] !== b[key] && hasOwnProperty.call(b, key) ) {
+      return false;
+    }
+  }
+  for (const key in a) {
+    if ( a[key] !== b[key] && hasOwnProperty.call(a, key) ) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
 // Returns a clone of original object with only the specified keys
 // Returns the original if nothing changed.
 const objectOnly = (original, keys) => {
@@ -111,18 +133,29 @@ const objectWithout = (original, keys) => {
 };
 
 
+// Returns original if replacement has the same keys + values.
+// Otherwise returns replacement as is.
+const objectReplace = (original, replacement) => {
+    return objectIsSame(original, replacement) ? original : replacement;
+};
+
+
 
 const object = {
   clean: objectClean,
   isEmpty: objectIsEmpty,
+  isSame: objectIsSame,
   only: objectOnly,
+  replace: objectReplace,
   update: objectUpdate,
   without: objectWithout,
 };
 export {
   objectClean,
   objectIsEmpty,
+  objectIsSame,
   objectOnly,
+  objectReplace,
   objectUpdate,
   objectWithout,
 
