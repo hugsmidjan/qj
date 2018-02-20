1d70da1db17f62b8e79e3103142d3f469b57e40d
mar <mar.nospam@anomy.net>
2018-02-20 11:37:02 +0000

    refactor: Only resolve delimiter if queryString is non-empty

--- addUrlParams.js
+++ addUrlParams.js
@@ -4,7 +4,9 @@ export default function addUrlParams(url, paramsObj) {
   const hashUrl = url.split('#');
   url = hashUrl[0].replace(/\?$/, '');
   const hash = hashUrl[1] ? '#'+hashUrl[1] : '';
-  const delim = /\?/.test(url) ? '&' : '?';
-  const queryString = makeQueryString(paramsObj);
-  return url + (queryString.length ? delim : '') + queryString + hash;
+  let queryString = makeQueryString(paramsObj);
+  if ( queryString.length ) {
+    queryString = (/\?/.test(url) ? '&' : '?') + queryString;
+  }
+  return url + queryString + hash;
 }
