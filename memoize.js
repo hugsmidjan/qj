94ef7fb37cad8722cae5c9f3d1010b66cd3e218b
mar <mar.nospam@anomy.net>
2018-03-20 18:01:29 +0000

    feat: Add memoize function

---
+++ memoize.js
@@ -0,0 +1,53 @@
+// Simple, stupid, memory-efficient, and super fast memoizer.
+// Checks for strict equality with the last parameter values.
+const _memoizers = [
+    (fn) => fn,
+    (fn, value, p) => {
+        return function (a) {
+            if ( !p || a!==p[0] ) {
+                p = [a];
+                value = fn.apply(this, p);
+            }
+            return value;
+        };
+    },
+    (fn, value, p) => {
+        return function (a,b) {
+            if ( !p || a!==p[0] || b!==p[1] ) {
+                p = [a,b];
+                value = fn.apply(this, p);
+            }
+            return value;
+        };
+    },
+    (fn, value, p) => {
+        return function (a,b,c) {
+            if ( !p || a!==p[0] || b!==p[1] || c!==p[2] ) {
+                p = [a,b,c];
+                value = fn.apply(this, p);
+            }
+            return value;
+        };
+    },
+];
+const _memoizerN = (fn, value, p) => {
+    const arity = fn.length;
+    return function (...args) {
+        let i = 0;
+        while (i < arity) {
+            if (args[i] !== p[i]) {
+                p = args;
+                return (value = fn.apply(this, p));
+            }
+            i++;
+        }
+        return value;
+    };
+};
+
+const memoize = (fn) => {
+    const memoizer = _memoizers[fn.length] || _memoizerN;
+    return memoizer(fn);
+};
+
+export default memoize;
