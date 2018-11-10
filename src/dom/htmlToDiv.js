348b93f935295be2b511717940a52985dfde0901
mar <mar.nospam@anomy.net>
2018-11-10 11:11:38 +0000

    fix: Make html-disabling regexp safer

--- src/dom/htmlToDiv.js
+++ src/dom/htmlToDiv.js
@@ -8,8 +8,8 @@ export default function htmlToDiv(html/*:string*/, opts/*:?HtmlToDivOpts*/)/*:HT
     const div = document.createElement('div');
     div.innerHTML = String(html)
         .replace(/<!DOCTYPE[^>]*>/i, '')
-        .replace(/(<\/?(img|script|html|head|body|title|meta|style|link))/gi, (m,p1,p2) => {
-            return p1 + (opts && !opts['keep'+p2.toLowerCase()] ? '' : '--disabled');
+        .replace(/(<\/?(img|script|html|head|body|title|meta|style|link))( |>)/gi, (m,p1,p2,p3) => {
+            return p1 + (opts && opts['keep'+p2.toLowerCase()] ? '' : '--disabled') + p3;
         });
     return div;
 }
