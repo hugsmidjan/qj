c019ad91d3f0297625e8e5e4148c5a2a1c103ba5
mar <mar.nospam@anomy.net>
2018-11-12 01:47:32 +0000

    fix: Make closestParent work with SVG elements also

--- src/select/closestParent.js
+++ src/select/closestParent.js
@@ -2,11 +2,11 @@
 import matches from './matches';
 
 // TODO: Find a way to be more clever about the type annotation for the return value
-export default function closestParent(selector/*:string*/, elm/*:Node*/, stopper/*::?:HTMLElement*/)/*:null|HTMLElement*/ {
+export default function closestParent(selector/*:string*/, elm/*:Node*/, stopper/*::?:Element*/)/*:null|Element*/ {
     let _stopper = stopper || null;
     let candidateElm = elm instanceof Element ? elm : elm.parentElement;
     while ( candidateElm && !matches(selector, candidateElm) && candidateElm !== _stopper ) {
          candidateElm = candidateElm.parentElement;
     }
-    return candidateElm !== _stopper && candidateElm instanceof HTMLElement ? candidateElm : null;
+    return candidateElm !== _stopper && candidateElm instanceof Element ? candidateElm : null;
 }
