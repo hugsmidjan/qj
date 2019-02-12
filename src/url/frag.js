54da0ec5e1d88ae8e024d7da57b32d6cae9945a0
mar <mar.nospam@anomy.net>
2019-02-12 11:15:01 +0000

    style: Prettier format frag.js

--- src/url/frag.js
+++ src/url/frag.js
@@ -9,53 +9,53 @@
 //   setFrag(null);  // unset
 
 const setFrag = (_fragment, _isEncoded) => {
-  _fragment = (_fragment||'').replace(/^#/, '');
+  _fragment = (_fragment || '').replace(/^#/, '');
   // check if there exists an element with .id same as _fragment
-  var _elm = _fragment  &&  document.getElementById( _isEncoded ? decodeURIComponent(_fragment) : _fragment );
-  var _prePos = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
+  var _elm =
+    _fragment &&
+    document.getElementById(
+      _isEncoded ? decodeURIComponent(_fragment) : _fragment
+    );
+  var _prePos =
+    window.pageYOffset ||
+    document.documentElement.scrollTop ||
+    document.body.scrollTop;
   var _tmpId = _elm && _elm.id;
 
   // temporaily defuse the element's id
-  _elm  &&  (_elm.id = '');
+  _elm && (_elm.id = '');
 
   // set the damn hash... (Note: Safari 3 & Chrome barf if frag === '#'.)
-  document.location.hash = (_isEncoded ? _fragment : encodeFrag(_fragment) );
+  document.location.hash = _isEncoded ? _fragment : encodeFrag(_fragment);
 
   // Always reset scrollpos
   // (because Chrome ~v34 seems to scroll to the element which had -
   // that ID on page load regardless of wheter its id has changed
   // or if another element has now received that id. weird.)
-  window.scrollTo( 0, _prePos);
+  window.scrollTo(0, _prePos);
 
   // put the old DOM id back in it's place
-  _elm  &&  (_elm.id = _tmpId);
+  _elm && (_elm.id = _tmpId);
 };
-
 
 // encodes a plain-text string to a URL #fragment friendly format (compatible with .get())
-const encodeFrag = (_fragment) => {
-  return encodeURI(_fragment).replace(/#/g, '%23').replace(/%7C/g, '|');
+const encodeFrag = _fragment => {
+  return encodeURI(_fragment)
+    .replace(/#/g, '%23')
+    .replace(/%7C/g, '|');
 };
-
 
 // returns the #fragment portion of `url` (defaulting to using `document.location.href`)
 // returns a plaintext (decodeURIComponent) version of the fragment - unless a `_returnRaw` argument is passed.
 const getFrag = (url, _returnRaw) => {
-  var _fragment = ( url || document.location.href ).split('#')[1] || '';
+  var _fragment = (url || document.location.href).split('#')[1] || '';
   return _returnRaw ? _fragment : decodeURIComponent(_fragment);
 };
-
 
 const frag = {
   get: getFrag,
   set: setFrag,
-  encode: encodeFrag,
+  encode: encodeFrag
 };
 
-export {
-  setFrag,
-  getFrag,
-  encodeFrag,
-
-  frag as default,
-};
+export { setFrag, getFrag, encodeFrag, frag as default };
