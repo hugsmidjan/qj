d3ceb928a91682efea0dc3dbd3ac2f9812c16843
mar <mar.nospam@anomy.net>
2019-04-24 15:49:45 +0000

    style: Prettier formatting

--- src/__polyfills/Object.values.js
+++ src/__polyfills/Object.values.js
@@ -1,4 +1,7 @@
-Object.values = Object.values || function (object) {
-    return Object.keys(object)
-        .map(function (key) { return object[key]; });
-};
+if (!Object.values) {
+  Object.values = function (object) {
+    return Object.keys(object).map(function (key) {
+      return object[key];
+    });
+  };
+}
