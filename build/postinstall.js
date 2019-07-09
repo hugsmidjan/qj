b3c59b8d0da436eaa845086d2a9a15b2368b622b
mar <mar.nospam@anomy.net>
2019-07-09 15:43:33 +0000

    refactor: Remove redundant equality check (obviated by indexOf)

--- build/postinstall.js
+++ build/postinstall.js
@@ -4,12 +4,10 @@ const initCwd = env.INIT_CWD
 if (
     // This seems to happen when running `yarn install` on a parent project (installing qj as a dependency)
     // on Win10's Linux sub-system.
-    typeof initCwd === 'undefined' || (
-    // if INIT_CWD (yarn/npm install invocation) and PWD are the same, then local (dev) install/add is taking place.
-    initCwd !== env.PWD &&
+    typeof initCwd === 'undefined' ||
+    // if INIT_CWD (yarn/npm install invocation) and PWD are the same, then local (dev) install/add is taking place
     // local (dev) yarn install may have been run from a project subfolder
     initCwd.indexOf(env.PWD) !== 0
-    )
 ) {
   if (typeof initCwd === 'undefined') {
     console.warn('process.env.INIT_CWD is undefined.');
