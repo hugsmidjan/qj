fe1a7c53d8f481f3940f30ddc0c0380f40284209
mar <mar.nospam@anomy.net>
2019-11-18 16:27:44 +0000

    fix: `removeNode()` should accept `null`

--- src/dom/removeNode.ts
+++ src/dom/removeNode.ts
@@ -1,4 +1,7 @@
-export default function removeNode<N extends Node>(node: N): N {
-	node.parentNode && node.parentNode.removeChild(node);
+export default function removeNode(node: null): null;
+export default function removeNode<N extends Node>(node: N): N;
+
+export default function removeNode<N extends Node>(node: N | null): N | null {
+	node && node.parentNode && node.parentNode.removeChild(node);
 	return node;
 }
