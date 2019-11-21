1134be1da5c2dea8987ee6ebb7352df572b6ac7f
mar <mar.nospam@anomy.net>
2019-11-21 14:07:48 +0000

    feat: Add `polyfills/Object.fromEntries`

---
+++ src/__polyfills/Object.fromEntries.js
@@ -0,0 +1,9 @@
+if (!Object.fromEntries) {
+	Object.fromEntries = function(list) {
+		const obj = {};
+		[].slice.call(list).forEach((entry) => {
+			obj[entry[0]] = entry[1];
+		});
+		return obj;
+	};
+}
--- src/__polyfills/Object.js
+++ src/__polyfills/Object.js
@@ -1,3 +1,4 @@
 import './Object.assign';
 import './Object.values';
 import './Object.entries';
+import './Object.fromEntries';
