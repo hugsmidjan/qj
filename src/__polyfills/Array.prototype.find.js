71e98d534a59f00515ff852a712de230482feeb4
mar <mar.nospam@anomy.net>
2019-11-21 14:25:26 +0000

    style: Clean up `polyfills/Array.prototype.find`

--- src/__polyfills/Array.prototype.find.js
+++ src/__polyfills/Array.prototype.find.js
@@ -1,11 +1,11 @@
 if (!Array.prototype.find) {
 	Array.prototype.find = function(predicate) {
-		const context = arguments[1];
+		const list = this;
 		let i = 0;
-		const l = this.length;
+		const l = list.length;
 		while (i < l) {
-			const item = this[i];
-			if (predicate.call(context, item, i, this)) {
+			const item = list[i];
+			if (predicate.call(arguments[1], item, i, list)) {
 				return item;
 			}
 			i++;
