e05634e65472ab636c5ae2f01e6bcd7ad9707221
mar <mar.nospam@anomy.net>
2020-03-02 06:41:31 +0000

    style: Add spacing and remove stray clearTimeout call

--- src/functions/debounce.ts
+++ src/functions/debounce.ts
@@ -11,24 +11,27 @@ const debounce = <F extends (...args: any) => void>(
 	immediate?: boolean
 ): Cancellable<F> => {
 	let timeout: TimerId | undefined;
+
 	const debouncedFn = function(...args) {
 		const runNow = immediate && !timeout;
 		const _this = this; // eslint-disable-line @typescript-eslint/no-this-alias
+
 		timeout && clearTimeout(timeout);
 		timeout = setTimeout(() => {
 			!runNow && func.apply(_this, args); // don't re-invoke `func` if runNow is true
 			timeout = undefined;
 		}, delay);
+
 		runNow && func.apply(_this, args);
 	} as Cancellable<F>;
+
 	debouncedFn.cancel = () => {
 		timeout && clearTimeout(timeout);
 		timeout = undefined;
 	};
+
 	return debouncedFn;
 };
-
-clearTimeout(setTimeout(() => {}, 1000));
 
 // Sugar to produce a dynamic debounced function
 // that accepts its contents/behavior at call time.
