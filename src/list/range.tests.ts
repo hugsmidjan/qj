7dd3f81a30dcf086b5b9bae4256ea32d10b37fc4
mar <mar.nospam@anomy.net>
2020-05-06 21:44:00 +0000

    docs: Add JavaDoc and re-order/re-group the tests for `range()`

--- src/list/range.tests.ts
+++ src/list/range.tests.ts
@@ -7,8 +7,6 @@ o.spec('range', () => {
 		o(range(10, 14)).deepEquals([10, 11, 12, 13, 14]);
 		o(range(-2, 2)).deepEquals([-2, -1, 0, 1, 2]);
 		o(range(1, 5, 2)).deepEquals([1, 3, 5]);
-		o(range(1, 5, 3)).deepEquals([1, 4]);
-		o(range(1, 5, 100)).deepEquals([1]);
 		o(range(1, 1)).deepEquals([1]);
 	});
 
@@ -17,6 +15,13 @@ o.spec('range', () => {
 		o(range(14, 10)).deepEquals([14, 13, 12, 11, 10]);
 		o(range(2, -2)).deepEquals([2, 1, 0, -1, -2]);
 		o(range(5, 1, 2)).deepEquals([5, 3, 1]);
+	});
+
+	o('Last value is limited by `step`', () => {
+		// I.e. the `step` size is fixed the last value
+		// never overshoots the limit set by the `to` parameter
+		o(range(1, 5, 3)).deepEquals([1, 4]);
+		o(range(1, 5, 100)).deepEquals([1]);
 		o(range(5, 1, 3)).deepEquals([5, 2]);
 		o(range(5, 1, 100)).deepEquals([5]);
 	});
--- src/list/range.ts
+++ src/list/range.ts
@@ -1,3 +1,11 @@
+/**
+ * Creates a Array containing integers ranging between `from` and `to`.
+ * Ranges can be either ascending or descending.
+ *
+ * @param from Starting value
+ * @param to End value
+ * @param step Positive number. Defaults to `1`
+ */
 const range = (from: number, to: number, step?: number) => {
 	const asc = from < to;
 	step = (asc ? 1 : -1) * Math.max(step || 0, 1);
