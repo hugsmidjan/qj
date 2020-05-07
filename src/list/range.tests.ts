86ba7170fcfad6a7bbd847d038765bafc3b70d78
mar <mar.nospam@anomy.net>
2020-05-07 11:41:50 +0000

    tests: Annotate range() tests better

--- src/list/range.tests.ts
+++ src/list/range.tests.ts
@@ -3,18 +3,17 @@ import range from './range';
 
 o.spec('range', () => {
 	o('makes basic ranges', () => {
-		o(range(1, 4)).deepEquals([1, 2, 3, 4]);
-		o(range(10, 14)).deepEquals([10, 11, 12, 13, 14]);
-		o(range(-2, 2)).deepEquals([-2, -1, 0, 1, 2]);
-		o(range(1, 5, 2)).deepEquals([1, 3, 5]);
-		o(range(1, 1)).deepEquals([1]);
+		o(range(1, 4)).deepEquals([1, 2, 3, 4])('makes a range');
+		o(range(10, 14)).deepEquals([10, 11, 12, 13, 14])('accepts arbitrary from values');
+		o(range(-2, 2)).deepEquals([-2, -1, 0, 1, 2])('can start from a negative value');
+		o(range(1, 5, 2)).deepEquals([1, 3, 5])('handles custom steps');
+		o(range(1, 1)).deepEquals([1])('Tolerates from and to being the same');
 	});
 
 	o('makes descending ranges', () => {
-		o(range(4, 1)).deepEquals([4, 3, 2, 1]);
-		o(range(14, 10)).deepEquals([14, 13, 12, 11, 10]);
-		o(range(2, -2)).deepEquals([2, 1, 0, -1, -2]);
-		o(range(5, 1, 2)).deepEquals([5, 3, 1]);
+		o(range(5, 3)).deepEquals([5, 4, 3])('makes a range');
+		o(range(2, -2)).deepEquals([2, 1, 0, -1, -2])('safely crosses into negative values');
+		o(range(5, 1, 2)).deepEquals([5, 3, 1])('handles custom steps');
 	});
 
 	o('Last value is limited by `step`', () => {
@@ -26,13 +25,15 @@ o.spec('range', () => {
 		o(range(5, 1, 100)).deepEquals([5]);
 	});
 
+	o('Throws on invalid/NaN `to`/`from` inputs', () => {
 	o('invalid steps are ignored', () => {
+		// zero step size is just silly
 		o(range(1, 4, 0)).deepEquals([1, 2, 3, 4]);
+		// NOTE: decending ranges MUST be expressed by `from > to`.
+		// Negative `step` values are ignored
 		o(range(1, 4, -1)).deepEquals([1, 2, 3, 4]);
-		o(range(1, 4, NaN)).deepEquals([1, 2, 3, 4]);
-
-		o(range(4, 1, 0)).deepEquals([4, 3, 2, 1]);
 		o(range(4, 1, -1)).deepEquals([4, 3, 2, 1]);
-		o(range(4, 1, NaN)).deepEquals([4, 3, 2, 1]);
+		// gracefully ignore NaN
+		o(range(1, 4, NaN)).deepEquals([1, 2, 3, 4]);
 	});
 });
