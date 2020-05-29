5373508def2d885b9ee50beddc95f14ef2114b07
Valur Sverrisson <valur@hugsmidjan.is>
2020-05-29 15:52:00 +0000

    fix: convert to typescript

---
+++ src/browser/getScrollbarWidth.ts
@@ -0,0 +1,14 @@
+const doc = document;
+
+export default function getScrollbarWidth(): number {
+	if (!(doc && doc.body)) {
+		return 0;
+	}
+	const scrollDiv = doc.createElement('div');
+	scrollDiv.style.cssText =
+		'position:absolute;top:-9999px;width:50px;height:50px;overflow:scroll;';
+	doc.body.appendChild(scrollDiv);
+	const scrollbarWidth = scrollDiv.getBoundingClientRect().width - scrollDiv.clientWidth;
+	doc.body.removeChild(scrollDiv);
+	return scrollbarWidth;
+}
