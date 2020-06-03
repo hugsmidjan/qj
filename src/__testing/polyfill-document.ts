811db521144c3211d481d3541b69194ae0f274fd
mar <mar.nospam@anomy.net>
2020-06-03 14:32:42 +0000

    style: Drop @ts-ignore

--- src/__testing/polyfill-document.ts
+++ src/__testing/polyfill-document.ts
@@ -1,6 +1,6 @@
 import jsdom from 'jsdom';
-// @ts-ignore  (we're willfully extending the global object)
-if (!global.window) {
+
+if (!('window' in global)) {
 	const window = new jsdom.JSDOM('<html/>').window;
 	// @ts-ignore  (we're willfully extending the global object)
 	global.window = window.window;
