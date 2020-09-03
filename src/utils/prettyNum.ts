b9ca92644cfe5d6a9b4d12d981651a11e06b6bfe
Valur Sverrisson <valur@hugsmidjan.is>
2020-09-03 17:53:04 +0000

    fix: use object for opts, add language support and add more tests

--- src/utils/prettyNum.ts
+++ src/utils/prettyNum.ts
@@ -1,14 +1,50 @@
 // Chrome doesn't yet support icelandic for Number.toLocaleString (2.9.2020)
-export const prettyNum = (
-	number?: string | number,
-	decimals = 0,
-	tSep = '.',
-	dSep = ','
-) => {
-	number = typeof number === 'string' ? parseFloat(number) : number;
+const i18n = {
+	is: [',', '.'],
+	en: ['.', ','],
+} as const;
+type SupportedLanguages = keyof typeof i18n;
+
+export type PrettyNumOptions = {
+	/** Max number of decimals to render */
+	decimals?: number;
+	/** Should decimals be uniformly applied to all numbers */
+	// fixedDecimals?: boolean;
+	/** Default: `true` */
+	leadingZero?: boolean;
+	/** Default: `'en'`
+	 *
+	 * If your language isn't supported use the `splitters` option instead.
+	 */
+	lang?: SupportedLanguages;
+	/** The tokens to use for a) decimals and b) thousands
+	 *
+	 * Examples Icelandic uses `[',', '.']` and English uses `['.', ',']`
+	 *
+	 * Default: `['.', '']`
+	 */
+	splitters?: [string, string] | [string];
+	// Supported by TS 4.0 https://devblogs.microsoft.com/typescript/announcing-typescript-4-0-rc/#labeled-tuple-elements
+	// splitters?: [decimal: string, thousands: string] | [decimal: string];
+};
+
+export const prettyNum = (number?: string | number, opts: PrettyNumOptions = {}) => {
+	const {
+		decimals = 0, // is this the right default?
+		// fixedDecimals,
+		leadingZero = true,
+		lang = 'en',
+		// NOTE: explicit `splitters` value always trumps the `lang` option
+		splitters = i18n[lang] || [],
+	} = opts;
+	number = parseFloat('' + number);
 	number = !number || isNaN(number) ? 0 : number;
 
+	const dSep = splitters[0] || '.';
+	const tSep = splitters[1] || '';
+
 	const numSplit = number.toFixed(decimals).split('.');
-	const num = numSplit[0].replace(/\d(?=(\d{3})+$)/g, '$&' + tSep);
+	let num = numSplit[0].replace(/\d(?=(\d{3})+$)/g, '$&' + tSep);
+	num = !leadingZero && num[0] === '0' ? num.replace(/\d/, '') : num;
 	return num + (numSplit[1] ? dSep + numSplit[1] : '');
 };
