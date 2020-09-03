33631a1df4aa42991c790d8d6f71dca261179745
Valur Sverrisson <valur@hugsmidjan.is>
2020-09-03 10:37:21 +0000

    fix: make number more tolerant to invalid inputs

--- src/utils/prettyNum.tests.ts
+++ src/utils/prettyNum.tests.ts
@@ -1,10 +1,13 @@
 import o from 'ospec';
 import { prettyNum } from './prettyNum';
 
-o.spec('regEscape()', () => {
-	o('Escapes strings', () => {
+o.spec('prettyNum()', () => {
+	o('Prettify numbers', () => {
 		o(prettyNum()).equals('0');
+		o(prettyNum(NaN)).equals('0');
+		o(prettyNum('bob')).equals('0');
 		o(prettyNum(42)).equals('42');
+		o(prettyNum('42')).equals('42');
 		o(prettyNum(123456789)).equals('123.456.789');
 		o(prettyNum(123456.789, 2, '.', ',')).equals('123.456,79');
 		o(prettyNum(1234.56789, 3, ',', '.')).equals('1,234.568');
--- src/utils/prettyNum.ts
+++ src/utils/prettyNum.ts
@@ -1,5 +1,13 @@
 // Chrome doesn't yet support icelandic for Number.toLocaleString (2.9.2020)
-export const prettyNum = (number = 0, decimals = 0, tSep = '.', dSep = ',') => {
+export const prettyNum = (
+	number?: string | number,
+	decimals = 0,
+	tSep = '.',
+	dSep = ','
+) => {
+	number = typeof number === 'string' ? parseFloat(number) : number;
+	number = !number || isNaN(number) ? 0 : number;
+
 	const numSplit = number.toFixed(decimals).split('.');
 	const num = numSplit[0].replace(/\d(?=(\d{3})+$)/g, '$&' + tSep);
 	return num + (numSplit[1] ? dSep + numSplit[1] : '');
