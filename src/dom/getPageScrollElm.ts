14716b0493662af9425b2fff677970f2a38f17f0
mar <mar.nospam@anomy.net>
2021-12-03 09:43:54 +0000

    feat: Make `getPageScrollElm` prefer `<html/>` and add JSDoc

--- src/dom/getPageScrollElm.ts
+++ src/dom/getPageScrollElm.ts
@@ -1,9 +1,18 @@
-const getPageScrollElm = () => {
-	let scrollElm = document.body;
-	if (scrollElm.clientHeight === scrollElm.scrollHeight) {
-		scrollElm = document.documentElement;
+/**
+ * Returns the one of `<html/>` or `<body/>` which seems to be
+ * responsible for "scrolling the page", preferring `<html />`.
+ */
+const getPageScrollElm = (customWindow: Window = window) => {
+	const document = customWindow.document;
+	const bodyElm = document.body;
+	let pageScrollElm = document.documentElement;
+	if (
+		pageScrollElm.clientHeight === pageScrollElm.scrollHeight &&
+		bodyElm.clientHeight !== bodyElm.scrollHeight
+	) {
+		pageScrollElm = bodyElm;
 	}
-	return scrollElm;
+	return pageScrollElm;
 };
 
 export default getPageScrollElm;
