5dbdd143c1c6cfa9752fce78cdbab6f0080c4cad
Már Örlygsson <mar.nospam@anomy.net>
2023-03-08 15:47:41 +0000

    chore: Reinstall deps with node 16 - upgrade "hxmstyle" to latest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,12 @@
 /* eslint-env node */
 module.exports = require('@hugsmidjan/hxmstyle')({
-	// Place your project-specific additions or overrides here
-	// using standard ESLint config syntax...
+  // Place your project-specific additions or overrides here
+  // using standard ESLint config syntax...
 
-	env: {
-		browser: true,
-		es2020: true,
-	},
+  // extendsFirst: [], // extended BEFORE the hxmstyle rules
+  // extends: [], // added after the hxmstyle rules
+  env: {
+    browser: true,
+    es2020: true,
+  },
 });
