57ec0ad4c838008097727c228c4f32ff9c10b0b7
Már Örlygsson <mar.nospam@anomy.net>
2024-03-06 11:18:59 +0000

    refactor: Minor code-juggling, add JSDocs

--- src/list/alphabetize.ts
+++ src/list/alphabetize.ts
@@ -1,6 +1,9 @@
 import sortIsl from './sortIsl';
 
-let supportsIcelandic: boolean;
+let supportsIcelandic: boolean | undefined;
+
+const envHasIcelandicSupport = () =>
+  'ð'.localeCompare('e', 'is') < 0 && 'ob'.localeCompare('öa', 'is') < 0;
 
 const langAliases: Record<string, string> = {
   no: 'nb', // 'no' doesn't work in Chrome, but 'nb' does
@@ -8,14 +11,31 @@ const langAliases: Record<string, string> = {
 
 type ToStringer<T> = (item: T) => string;
 
+/**
+ * Sorts an array of strings or objects in alpha1betic order, using
+ * `.localeCompare()` by default but using a custom function for Icelandic
+ * sorting in Chrome (as long as Chrome doesn't support Icelandic sorting).
+ *
+ * It also fixes a known issue in Chrome where lang="no" doesn't work but
+ * lang="nb" does.
+ */
 function alphabetize<T extends object, K extends keyof T>(
+  /** The array to sort */
   arr: Array<T>,
+  /** The two-letter language code to sort by */
   lang: string,
+  /** The name of the item key to sort by */
   getProp?: K
 ): Array<T>;
 function alphabetize<T extends object | string | number>(
+  /** The array to sort */
   arr: Array<T>,
+  /** The two-letter language code to sort by */
   lang: string,
+  /**
+   * Callback that returns the string representation of each item in the array
+   * to sort by.
+   */
   getProp?: ToStringer<T>
 ): Array<T>;
 
@@ -34,20 +54,20 @@ function alphabetize<T extends string | number | object, K extends keyof T>(
   if (
     lang !== 'is' ||
     supportsIcelandic ||
-    (supportsIcelandic !== false && // This means it's not tested yet
-      !!(supportsIcelandic =
-        'ð'.localeCompare('e', 'is') < 0 && 'ob'.localeCompare('öa', 'is') < 0))
+    (supportsIcelandic === undefined && // This means it's not tested yet
+      (supportsIcelandic = envHasIcelandicSupport()))
   ) {
-    const newArr = arr.map((item, idx) => ({ value: _getProp(item), idx }));
     lang = langAliases[lang] || lang;
-    newArr.sort((a, b) => {
-      return a.value.localeCompare(b.value, lang, {
-        sensitivity: 'accent',
-        ignorePunctuation: true,
-        numeric: true,
-      });
-    });
-    return newArr.map((item) => arr[item.idx]);
+    return arr
+      .map((item) => ({ value: _getProp(item), item }))
+      .sort((a, b) =>
+        a.value.localeCompare(b.value, lang, {
+          sensitivity: 'accent',
+          ignorePunctuation: true,
+          numeric: true,
+        })
+      )
+      .map((item) => item.item);
   } else {
     return sortIsl(arr, { getProp: _getProp });
   }
